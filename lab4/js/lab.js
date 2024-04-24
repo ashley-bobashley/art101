21acf50a3f4286f26bc79a9d698a2cd2ff5f4269
ashley-bobashley <christophermstelljes@gmail.com>
2024-04-23 19:56:57 -0700

    lab4 implementation

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Rock, Paper, Scissors
+//An implementation of the game
+//Ashley Stelljes <cstellje@ucsc.edu>
+//April 23 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Create the variables rock, paper, and scissors
+//Choose 1 randomly
+//Let user input their choice between rock, paper, or scissors
+//Compare the two variables
+	//If values are the same, print both players’ choices and the text “It’s a tie!”, then the computer and player both choose again
+	//If values are different, then:
+		//If player has rock:
+			//If computer has scissors: print both players’ choices and the text “You Win!”
+			//If computer has paper: print both players’ choices and the text “You Lose!”
+		//If player has scissors:
+			//If computer has paper: print both players’ choices and the text “You Win!”
+			//If computer has rock: print both players’ choices and the text “You Lose!”
+		//If player has paper:
+			//If computer has rock: print both players’ choices and the text “You Win!”
+			//If computer has scissors: print both players’ choices and the text “You Lose!”
