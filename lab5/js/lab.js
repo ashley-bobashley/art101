96015388b70ccbd86fb867a4812317467aa0e74d
ashley-bobashley <christophermstelljes@gmail.com>
2024-04-25 17:31:35 -0700

    added lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Ashley Stelljes <cstellje@ucsc.edu>
+//April 23 2024
 
-// Constants
+//Define Variables
+let make = "Kia";
+let model = "Forte";
+let year = "2016";
+let color = "Grey";
+let ownIt = true;
 
-// Functions
+//calculate
+let age = (2024 - year);
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: "+ make + "<br>");
+document.writeln("Model: "+ model + "<br>");
+document.writeln("Year: "+  year + "<br>");
+document.writeln("Color: "+ color + "<br>");
+document.writeln("Do I own the car? "+ ownIt + "<br>");
+document.writeln("Age: "+ age + " years old." + "<br>");
