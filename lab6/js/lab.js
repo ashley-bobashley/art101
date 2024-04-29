2cd5e546eb9f87886ee751c8e3b4b60a6d1a5a2a
ashley-bobashley <christophermstelljes@gmail.com>
2024-04-28 20:19:14 -0700

    added lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Ashley Stelljes <cstellje@ucsc.edu>
+//April 23 2024
 
-// Constants
+//Define Variables
+const myTransport = [Kia Forte, Bus, Walking];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//Define Objects
+const myMainRide = {
+  make: "Kia",
+  model: "Forte",
+  year: 2016,
+  color: "grey",
+  age: function() {
+    return 2024 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Code
+document.writeln("Getting Around" + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
