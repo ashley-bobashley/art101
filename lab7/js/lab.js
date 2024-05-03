1e3e91b44cbc1a395cb5dbef92037c1fb402f0c1
ashley-bobashley <christophermstelljes@gmail.com>
2024-05-02 19:36:55 -0700

    added lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+//Ashley Stelljes <cstellje@ucsc.edu>
+//April 23 2024
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//This function takes input from the user, generally a name, then sorts it and outputs the sorted version.
+function inputAlphabetizer() {
+  //Get input and verify
+  let userName = prompt("Please enter some text you'd like to be alphabetized");
+  console.log("username = ", userName);
+
+  //Convert string to array and verify
+  let nameArray = userName.split('');
+  console.log("nameArray = ", nameArray);
+
+  //Sort the array and verify
+  let nameArraySorted = nameArray.sort();
+  console.log("nameArraySorted = ", nameArraySorted);
+
+  //Convert sorted array back into a string
+  let nameSorted = nameArraySorted.join('');
+  console.log("nameSorted = ", nameSorted);
+
+  //Output result
+  return nameSorted;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Begin Program Code
+document.writeln("Your input has been alphabetized: ", inputAlphabetizer(), "</br>");
