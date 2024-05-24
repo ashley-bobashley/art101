5316fd9d1180d0153aa6aa48f2f40c56a0065f6c
ashley-bobashley <christophermstelljes@gmail.com>
2024-05-23 18:51:32 -0700

    added lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,21 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Ashley Stelljes <cstellje@ucsc.edu>
+//May 23th 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Declare Functions
+// This function returns a Hogwarts class based on the number of characters in the inputted string.
+function sortingHat(string){
+  // Counts the number of characters in a string
+  let length = string.length;
+  let mod = length % 4;
+  // Compare length to determine class
+  if (mod==0){
+    return "Gryffindor";
+  } else if (mod==1){
+    return "Ravenclaw";    
+  } else if (mod==2){
+    return "Slytherin";    
+  } else if (mod==3){
+    return "Hufflepuff";    
+  } else {
+    return "Error";
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Calls back to the sortingHat function when a button is pressed
+$("#button").click(function(){
+  // Get user input and store it as a constant
+  const input = $("#input").val();
+  // Assigns the input a class
+  const house = sortingHat(input);
+  // Reveals the input's class and appends it to the #output div
+  $("#output").append('<div class="text"><p>' + "The Sorting Hat has sorted you into " + house + '</p></div>');
+});
